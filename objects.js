1a535bc404105911c40443092480b39bfac4a1bd
Nikhil-yadav-kumar <nikhilyadav72646@gmail.com>
2026-02-09 14:50:16 +0530

    1st

---
+++ objects.js
@@ -0,0 +1,17 @@
+// let student={
+//     first_name:"Nikhil",
+//     last_name:"Yadav",
+//     age:20,
+//     address:"jharkhand",
+//     skill:{
+//         skill1:"graphic",
+//         skill2:"web developer"
+//     }
+// };
+// console.log(student)
+
+const student={};
+student.name="Nikhil";
+student.roll_no=34;
+student.address="jharkhand";
+console.log(student);
