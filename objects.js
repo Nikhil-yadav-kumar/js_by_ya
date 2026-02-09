2a9cc252276f5e51aa6476250c8e701cc54a5cc8
Nikhil-yadav-kumar <nikhilyadav72646@gmail.com>
2026-02-09 14:57:39 +0530

    accessing

--- objects.js
+++ objects.js
@@ -10,8 +10,22 @@
 // };
 // console.log(student)
 
-const student={};
-student.name="Nikhil";
-student.roll_no=34;
-student.address="jharkhand";
-console.log(student);
+// const student={};
+// student.name="Nikhil";
+// student.roll_no=34;
+// student.address="jharkhand";
+// console.log(student);
+
+let student={
+    first_name:"Nikhil",
+    last_name:"Yadav",
+    age:20,
+    address:"jharkhand",
+    skill:{
+        skill1:"graphic",
+        skill2:"web developer"
+    }
+};
+console.log(student.first_name);
+console.log(student["last_name"]);
+
